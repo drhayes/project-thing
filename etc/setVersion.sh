053d922823b6df555d7212d629625ba5f7b80458
David Hayes <drhayes@hey.com>
2022-08-08 09:36:15 -0500

    Bump the version for the filename changes.

--- etc/setVersion.sh
+++ etc/setVersion.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env fish
 
-set pluginVersion (jq '.version' './src/$__plugins_drhayes_project-thing_version.json')
+set pluginVersion (jq '.version' './src/version.json')
 jq ".version = $pluginVersion" package.json | sponge package.json
 jq ".version = $pluginVersion" src/plugin.info | sponge src/plugin.info
